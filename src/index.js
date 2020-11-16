5802c4b4fd36f6ce4fb45f4a55a634159767a394
edwin.sanchez <edwin.s.g@hotmail.com>
2020-11-16 17:09:02 -0500

    Inicio del curso

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const user ={
+  firstName: 'Edwin ',
+  lastName: 'Sanchez',
+  avatar:'https://s.gravatar.com/avatar/9ad96a9864bad9509f414bdd8002a228?s=80'
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function getName(user){
+  return `${user.firstName}${user.lastName}`
+}
+
+function getGreeting(user){
+  if (user){
+    return( 
+     <h1>{getName(user)}</h1>
+    )
+  }
+  
+  return<h1>Hello Stranger</h1>
+  
+}
+
+const element = (
+  <div>
+    <h1>{getGreeting(user)}</h1>
+    <img src={user.avatar} />
+  </div>
+) 
+
+const container = document.getElementById('root')
+
+ReactDOM.render(element, container)
